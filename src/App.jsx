ef12cb525be45b1e06d68932e836ef36a0628106
jey <diolite.jn@gmail.com>
2022-01-29 23:32:45 +0900

    P154
    CSS ModulesをDart Sassで実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import { ColordMeesge } from "./components/ColoredMessage";
+import "ress";
+import classes from "./scss/style.module.scss";
+
 export const App = () => {
+    
+    console.log("レンダリング");
+
+    const [ num , setNum ] = useState(0);
+    
+    useEffect(() => {
+        console.log("レンダリングB");
+    }, [num] );
+
     //ボタンを押したときの処理
     const onClickButton = () => {
-        alert();
+        setNum(( num ) => num + 1 ) ; 
     }
 
     const pinkStyle = {
@@ -10,12 +24,12 @@ export const App = () => {
         fontSize:"20px"
     }
     return (
-        <>
-        {console.log("TEST")}
+        <div className={classes["l-container"]}>
         <h1 style={{color : "red"}}>挨拶</h1>
         <ColordMeesge color="red" fontSize="72px">お元気ですか？</ColordMeesge>
         <p style={pinkStyle}>おげんきです</p>
         <button onClick={onClickButton}>ボタン</button>
-      </>
+        <p>{num}</p>
+      </div>
     )
 }
