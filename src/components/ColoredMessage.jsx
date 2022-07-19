093019a31df27f5c8b50d992fd733c273e69c695
jey <diolite.jn@gmail.com>
2022-07-19 23:35:46 +0900

    リセット

--- src/components/ColoredMessage.jsx
+++
@@ -1,8 +0,0 @@
-export const ColordMeesge = (props) => {
-    const {color , fontSize , children } = props;
-    const blueStyle = {
-        color,
-        fontSize
-    }
-    return <p style={blueStyle}>{children}</p>
-}
