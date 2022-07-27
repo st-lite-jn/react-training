d4b1e0e12539e1ff6b80677b3a02661aaf47c376
jey <diolite.jn@gmail.com>
2022-07-27 23:59:54 +0900

    P147まで Emotion実装まで完了

--- my-app/src/components/Emotion.jsx
+++ my-app/src/components/Emotion.jsx
@@ -1,36 +1,41 @@
-/** @jsxImortSource @emotion/react */
+/** コンポーネントファイルの中に CSS を書く方法 */
+/** @jsxImportSource @emotion/react */
+
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 
 export const Emotion = () => {
     // scssの書き方がそのまま可能な書き方
     const containerStyle = css`
-    width:600px;
-    margin-right:auto;
-    margin-left:auto;
-    border:solid 2px #999;
-`;
-    //インラインスタイルの書き方
+        border: solid 1px #aaa;
+        border-radius: 20px;
+        padding: 8px;
+        margin: 8px;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+  `;
+    // インラインスタイルの書き方
     const titleStyle = css({
-        fontSize:"36px",
-        marginTop:"40px",
-        color:"#999"
+        margin: 0,
+        color: "#aaa",
     });
 
-    const SButton = styled.button`
-    border: solid 2px yellow;
-    border-radius: 100vh;
-    background-color: white;
-    color: yellow;
-    &:hover {
-        border-color: green;
-        color: green;
-    }
-    `
     return (
         <div css={containerStyle}>
-            <p css={titleStyle}>Emoitonです</p>
+            <p css={titleStyle}>Emotion です </p>
             <SButton>ボタン</SButton>
         </div>
     )
+};
+// styled-componentsの書き方
+const SButton = styled.button`
+border: solid 2px yellow;
+border-radius: 100vh;
+background-color: white;
+color: yellow;
+&:hover {
+    border-color: green;
+    color: green;
 }
+`
